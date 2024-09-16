bec9cbee4cfebd1134c3e54b782de65e886c8926
Frame-Code <moraisurcan@hotmail.com>
2024-09-15 22:15:36 -0500

    Interfaz principal sin funcionalidad agregada

--- src/main/java/com/mycompany/gestion_alumnos/LOGICA/Gestion_Alumnos.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.gestion_alumnos.LOGICA;
-
-public class Gestion_Alumnos {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
---
+++ src/main/java/com/mycompany/gestion_alumnos/LOGICA/Main.java
@@ -0,0 +1,13 @@
+package com.mycompany.gestion_alumnos.LOGICA;
+
+import com.mycompany.gestion_alumnos.GUI.Principal;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Principal principal = new Principal();
+        principal.setVisible(true);
+        principal.setLocationRelativeTo(null);
+        
+    }
+}
