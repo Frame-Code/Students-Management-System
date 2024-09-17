c9036338f0a01a9fdf90837f02af746ce9a51c01
Frame-Code <moraisurcan@hotmail.com>
2024-09-17 17:13:10 -0500

    Agregado metodo para obtener edad en la Clase estudiante y demas cambios en dicha clase

--- src/main/java/com/mycompany/gestion_alumnos/LOGICA/Estudiante.java
+++ src/main/java/com/mycompany/gestion_alumnos/LOGICA/Estudiante.java
@@ -2,18 +2,18 @@ package com.mycompany.gestion_alumnos.LOGICA;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.Objects;
 
 /**
  *
  * Created by Frame-Code, September 2024
  */
 class Estudiante {
+
     private Long id;
     private String nombre;
     private LocalDate fecha_nacimiento;
-    private static final Integer edad = LocalDate.now().getYear();
+    private Integer edad;
     private Aula aula;
     private Matricula matricula;
     private List<PagoColegiatura> pago_colegiatura;
@@ -27,11 +27,121 @@ class Estudiante {
         this.aula = aula;
         this.matricula = matricula;
         this.pago_colegiatura = pago_colegiatura;
-        edad = (fe)
+        edad = agregarEdad();
+    }
+
+    private Integer agregarEdad() {
+        Integer edadGeneral = LocalDate.now().getYear() - this.fecha_nacimiento.getYear();
+        if (LocalDate.now().getMonthValue() < this.fecha_nacimiento.getMonthValue()) {
+            if (LocalDate.now().getDayOfMonth() < this.fecha_nacimiento.getDayOfMonth()) {
+                edadGeneral--;
+            }
+        }
+        return edadGeneral;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad() {
+        this.edad = this.agregarEdad();
     }
     
     
+    public LocalDate getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+    public void setFecha_nacimiento(LocalDate fecha_nacimiento) {
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public Aula getAula() {
+        return aula;
+    }
+    public void setAula(Aula aula) {
+        this.aula = aula;
+    }
+
+    public Matricula getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(Matricula matricula) {
+        this.matricula = matricula;
+    }
+
+    public List<PagoColegiatura> getPago_colegiatura() {
+        return pago_colegiatura;
+    }
+    public void setPago_colegiatura(List<PagoColegiatura> pago_colegiatura) {
+        this.pago_colegiatura = pago_colegiatura;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + Objects.hashCode(this.id);
+        hash = 23 * hash + Objects.hashCode(this.nombre);
+        hash = 23 * hash + Objects.hashCode(this.fecha_nacimiento);
+        hash = 23 * hash + Objects.hashCode(this.edad);
+        hash = 23 * hash + Objects.hashCode(this.aula);
+        hash = 23 * hash + Objects.hashCode(this.matricula);
+        hash = 23 * hash + Objects.hashCode(this.pago_colegiatura);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Estudiante other = (Estudiante) obj;
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.fecha_nacimiento, other.fecha_nacimiento)) {
+            return false;
+        }
+        if (!Objects.equals(this.edad, other.edad)) {
+            return false;
+        }
+        if (!Objects.equals(this.aula, other.aula)) {
+            return false;
+        }
+        if (!Objects.equals(this.matricula, other.matricula)) {
+            return false;
+        }
+        return Objects.equals(this.pago_colegiatura, other.pago_colegiatura);
+    }
+
+    @Override
+    public String toString() {
+        return "Estudiante{" + "id=" + id + ", nombre=" + nombre + ", fecha_nacimiento=" + fecha_nacimiento + ", edad=" + edad + ", aula=" + aula + ", matricula=" + matricula + ", pago_colegiatura=" + pago_colegiatura + '}';
+    }
     
     
-            
+
 }
